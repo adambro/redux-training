e5304ce79bf93de20584aed1683e08019861ab64
Rafał Kostrzewski <poczta.raf.kos@gmail.com>
2018-01-14 23:04:54 +0100

    Redux normalization example done

--- redux-shop/src/modules/cart/reducer.js
+++ redux-shop/src/modules/cart/reducer.js
@@ -9,14 +9,36 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+  let productId = action.productId
+  let existingQuantity = state[productId] || 0
+
   switch (action.type) {
     case ADD_PRODUCT:
+      return Object.assign({}, state, { [productId]: existingQuantity + action.quantity })
     case INCREASE_PRODUCT_QUANTITY:
+      return (productId in state)
+        ? Object.assign({}, state, { [productId]: existingQuantity + 1 })
+        : state
     case DECREASE_PRODUCT_QUANTITY:
+      if (!(productId in state)) {
+        return state
+      }
+      return existingQuantity > 1
+        ? Object.assign({}, state, { [productId]: existingQuantity -1 })
+        : withoutKey(state, productId)
     case REMOVE_PRODUCT:
+      return withoutKey(state, productId)
     default:
-     return state
+      return state
   }
 }
 
 export default reducer
+
+const withoutKey = (obj, key) => {
+  let copy = Object.assign({}, obj)
+
+  delete copy[key]
+  console.log('delete', key, 'before', obj, 'after', copy)
+  return copy
+}
--- redux-shop/src/modules/shop/reducer.js
+++ redux-shop/src/modules/shop/reducer.js
@@ -13,9 +13,54 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_PRODUCTS_SUCCEEDED:
+      return {
+        products: {
+          byId: getProductsById(action.products),
+          byCategoryId: getProductsByCategoryId(action.products)
+        },
+        categories: {
+          byId: getCategoriesById(action.products)
+        }
+      }
     default:
       return state
   }
 }
 
 export default reducer
+
+const getProductsById = products => {
+  return products.reduce(
+    (byId, product) => {
+      let categoryId = product.category.id
+      let normalizedProduct = Object.assign({}, product, { category: undefined, categoryId: categoryId })
+
+      return Object.assign(byId, { [product.id]: normalizedProduct })
+    },
+    {}
+  )
+}
+
+const getProductsByCategoryId = products => {
+  return products.reduce(
+    (byCategoryId, product) => {
+      let categoryId = product.category.id
+      let productsInCategory = byCategoryId[categoryId] || []
+
+      return Object.assign(byCategoryId, { [categoryId]: productsInCategory.concat(product.id) })
+    },
+    {}
+  )
+}
+
+const getCategoriesById = products => {
+  return products.map(p => p.category)
+    .reduce(
+      (byId, category) => {
+        let categoryId = category.id
+
+        return Object.assign(byId, { [categoryId]: category })
+      },
+      {}
+    )
+}
