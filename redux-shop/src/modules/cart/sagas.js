09e4f161d20a5c7897d51d1868293a7a49a2865e
Adam Brodziak <adambro@o2.pl>
2018-03-22 14:05:32 +0100

    WIP

--- redux-shop/src/modules/cart/sagas.js
+++ redux-shop/src/modules/cart/sagas.js
@@ -1,5 +1,15 @@
+import { take, race, call } from 'redux-saga/effects'
+import { delay } from 'redux-saga'
+import { CHECKOUT } from './constants'
+
 export default function * cartSaga () {
-  // TODO: Implement
+  const {checkout} = yield race({
+    checkout: yield take(CHECKOUT),
+    timeout: yield delay(1000)
+  })
+  if (!checkout) {
+    call(notifyProductsAddedToCartWithoutCheckout)
+  }
 }
 
 function notifyProductsAddedToCartWithoutCheckout () {
